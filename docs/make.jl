2e029f796fdc2aa69f3508074a61c5088192cb6d
jeffersonfparil <jeffersonparil@gmail.com>
2026-03-19 14:07:17 +1100

    downgrading LinearAlgebra version for compatibility with another package

--- docs/make.jl
+++ docs/make.jl
@@ -5,23 +5,23 @@ DocMeta.setdocmeta!(
     MultiLayerPerceptrons,
     :DocTestSetup,
     :(using MultiLayerPerceptrons);
-    recursive=true,
+    recursive = true,
 )
 
 makedocs(;
-    modules=[MultiLayerPerceptrons],
-    authors="jeffersonparil@gmail.com",
-    sitename="MultiLayerPerceptrons.jl",
-    format=Documenter.HTML(;
-        canonical="https://jeffersonfparil.github.io/MultiLayerPerceptrons.jl",
-        edit_link="main",
-        assets=String[],
-        size_threshold=1000000,
+    modules = [MultiLayerPerceptrons],
+    authors = "jeffersonparil@gmail.com",
+    sitename = "MultiLayerPerceptrons.jl",
+    format = Documenter.HTML(;
+        canonical = "https://jeffersonfparil.github.io/MultiLayerPerceptrons.jl",
+        edit_link = "main",
+        assets = String[],
+        size_threshold = 1000000,
     ),
-    pages=["Home" => "index.md"],
+    pages = ["Home" => "index.md"],
 )
 
 deploydocs(;
-    repo="github.com/jeffersonfparil/MultiLayerPerceptrons.jl",
-    devbranch="main",
+    repo = "github.com/jeffersonfparil/MultiLayerPerceptrons.jl",
+    devbranch = "main",
 )
